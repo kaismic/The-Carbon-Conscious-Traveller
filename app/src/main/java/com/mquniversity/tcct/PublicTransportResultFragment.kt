907e22629f7e12154cb2691ebbda3e550c7c21bd
Isac Kim <isac08270@gmail.com>
2023-12-21 23:20:42 +1100

    implemented icons list for travel steps

--- app/src/main/java/com/mquniversity/tcct/PublicTransportResultFragment.kt
+++ app/src/main/java/com/mquniversity/tcct/PublicTransportResultFragment.kt
@@ -1,14 +1,26 @@
 package com.mquniversity.tcct
 
+import android.content.res.ColorStateList
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.flexbox.FlexboxLayout
 import com.google.maps.model.DirectionsRoute
 import com.google.maps.model.TravelMode
 import com.google.maps.model.VehicleType
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.net.URL
 
 class PublicTransportResultFragment: ResultFragment() {
     override fun onCreateView(
@@ -32,6 +44,8 @@ class PublicTransportResultFragment: ResultFragment() {
             mainLayout,
             false) as LinearLayout
 
+        val stepsIconContainer: FlexboxLayout = resultLayout.findViewById(R.id.steps_icon_container)
+
         val emissionText: TextView = resultLayout.findViewById(R.id.emission_text)
         val distText: TextView = resultLayout.findViewById(R.id.distance_text)
         distText.text = route.legs[0].distance.humanReadable
@@ -45,6 +59,9 @@ class PublicTransportResultFragment: ResultFragment() {
         val steps = route.legs[0].steps
         var totalEmissionInGram = 0f
         for (step in steps) {
+            val nextIcon = ImageView(context).apply {
+                setImageResource(R.drawable.outline_navigate_next_24)
+            }
             when (step.travelMode) {
                 TravelMode.TRANSIT -> {
                     val factor: Float = when (step.transitDetails.line.vehicle.type) {
@@ -60,20 +77,74 @@ class PublicTransportResultFragment: ResultFragment() {
                         VehicleType.TRAM -> calculationValues.railValueMap["Light rail and tram"]!!
                         VehicleType.SUBWAY -> calculationValues.railValueMap["London Underground"]!!
                         VehicleType.FERRY -> calculationValues.ferryValueMap["Foot passenger"]!!
+                        VehicleType.TROLLEYBUS -> calculationValues.trolleybusValue
+                        VehicleType.CABLE_CAR -> calculationValues.cableCarValue
                         else -> 0f
                     }
                     totalEmissionInGram += step.distance.inMeters * factor
+
+                    val transitIconsContainer = LinearLayout(context).apply {
+                        orientation = LinearLayout.HORIZONTAL
+                        layoutParams = FlexboxLayout.LayoutParams(
+                            FlexboxLayout.LayoutParams.WRAP_CONTENT,
+                            FlexboxLayout.LayoutParams.WRAP_CONTENT
+                        )
+                    }
+                    // TODO use ConstraintLayout to make localIcon fit to shortName TextView
+                    try {
+                        var iconURL = step.transitDetails.line.vehicle.localIcon
+                        if (iconURL.isNullOrEmpty()) {
+                            iconURL = step.transitDetails.line.vehicle.icon
+                        }
+                        if (!iconURL.isNullOrEmpty()) {
+                            val completeURL = "https:$iconURL"
+                            val localIcon = ImageView(context)
+                            val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+                                throwable.printStackTrace()
+                            }
+                            CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+                                val bitmap = BitmapFactory.decodeStream(
+                                    URL(completeURL).openStream()
+                                )
+                                localIcon.post {
+                                    localIcon.setImageBitmap(bitmap)
+                                }
+                            }
+                            transitIconsContainer.addView(localIcon)
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    val shortNameText = step.transitDetails.line.shortName
+                    if (!shortNameText.isNullOrEmpty()) {
+                        val shortName = TextView(context).apply {
+                            background = ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.transport_short_name_shape,
+                                context.theme
+                            )
+                            setLines(1)
+                            text = shortNameText
+                            setTextColor(Color.parseColor(step.transitDetails.line.textColor))
+                            backgroundTintList = ColorStateList.valueOf(Color.parseColor(step.transitDetails.line.color))
+                        }
+                        transitIconsContainer.addView(shortName)
+                    }
+                    stepsIconContainer.addView(transitIconsContainer)
+                }
+                TravelMode.WALKING -> {
+                    val walkingIcon = ImageView(context).apply {
+                        setImageResource(R.drawable.outline_directions_walk_24)
+                    }
+                    stepsIconContainer.addView(walkingIcon)
                 }
                 else -> {}
             }
+            stepsIconContainer.addView(nextIcon)
         }
+        stepsIconContainer.removeViewAt(stepsIconContainer.childCount - 1)
         emissionText.text = CalculationUtils.formatEmission(totalEmissionInGram)
 
         mainLayout.addView(resultLayout)
     }
-
-    /*
-        TODO measure icon widths and
-         if current on exceeds LinearLayout width replace previous one with "..." TextView
-     */
 }

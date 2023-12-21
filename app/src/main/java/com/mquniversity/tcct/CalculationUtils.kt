0a54143ea6af437ae3ff737cce81e4a6359675ae
Isac Kim <isac08270@gmail.com>
2023-12-21 16:17:23 +1100

    added formatEmission method

--- app/src/main/java/com/mquniversity/tcct/CalculationUtils.kt
+++ app/src/main/java/com/mquniversity/tcct/CalculationUtils.kt
@@ -4,11 +4,15 @@ object CalculationUtils {
     /**
      * @return formatted text of CO2e emission in kilogram or gram
      */
-    fun calculateByDistance(distInMeter: Long, factor: Float): String {
-        val emission: Float = distInMeter * factor
-        if (emission > 1000) {
-            return String.format("%.2f", emission / 1000) + " kg CO2e"
+    fun calculateByDistance(distInMeter: Long, factorKgKm: Float): String {
+        val emissionInGram: Float = distInMeter * factorKgKm
+        return formatEmission(emissionInGram)
+    }
+
+    fun formatEmission(emissionInGram: Float): String {
+        if (emissionInGram >= 1000) {
+            return String.format("%.2f", emissionInGram / 1000) + " kg CO2e"
         }
-        return String.format("%.2f", emission) + " g CO2e"
+        return String.format("%.0f", emissionInGram) + " g CO2e"
     }
 }

5aa8656ae6454a0e8f856e1153fd9e1b370b8f1a
Isac Kim <isac08270@gmail.com>
2023-12-21 16:00:02 +1100

    fixed naming copied from old code

--- app/src/main/java/com/mquniversity/tcct/PrivateVehicleResultFragment.kt
+++ app/src/main/java/com/mquniversity/tcct/PrivateVehicleResultFragment.kt
@@ -1,6 +1,5 @@
 package com.mquniversity.tcct
 
-import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -16,8 +15,8 @@ open class PrivateVehicleResultFragment: ResultFragment() {
             mainLayout,
             false) as LinearLayout
 
-        val carIcon: ImageView = resultLayout.findViewById(R.id.transport_mode_icon)
-        carIcon.setImageDrawable(
+        val icon: ImageView = resultLayout.findViewById(R.id.transport_mode_icon)
+        icon.setImageDrawable(
             ResourcesCompat.getDrawable(
                 resources,
                 iconResId!!,
@@ -31,10 +30,8 @@ open class PrivateVehicleResultFragment: ResultFragment() {
         emissionText.text = CalculationUtils.calculateByDistance(route.legs[0].distance.inMeters, factor)
         val distText: TextView = resultLayout.findViewById(R.id.distance_text)
         distText.text = route.legs[0].distance.humanReadable
-        Log.i("distance testing", route.legs[0].distance.humanReadable)
         val durationText: TextView = resultLayout.findViewById(R.id.duration_text)
         durationText.text = route.legs[0].duration.humanReadable
-        Log.i("duration testing", route.legs[0].duration.humanReadable)
 
         emissionTexts.add(emissionText)
         distTexts.add(distText)

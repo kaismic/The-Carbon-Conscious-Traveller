b66eac12a41a18da33501551e5e09b528078a7e0
Isac Kim <isac08270@gmail.com>
2023-12-21 14:52:09 +1100

    change programmatic layout initialization to xml inflating

--- app/src/main/java/com/mquniversity/tcct/PrivateVehicleResultFragment.kt
+++ app/src/main/java/com/mquniversity/tcct/PrivateVehicleResultFragment.kt
@@ -1,95 +1,44 @@
 package com.mquniversity.tcct
 
-import android.util.TypedValue
-import android.view.Gravity
+import android.util.Log
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
-import com.google.android.material.textview.MaterialTextView
 import com.google.maps.model.DirectionsRoute
 
 open class PrivateVehicleResultFragment: ResultFragment() {
     protected var factor = 0f
 
     override fun insertRouteResult(route: DirectionsRoute, i: Int) {
-        val resultLayout = LinearLayout(context)
-        resultLayout.orientation = LinearLayout.HORIZONTAL
-        resultLayout.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
+        val resultLayout = layoutInflater.inflate(
+            R.layout.private_vehicle_result_item,
+            mainLayout,
+            false) as LinearLayout
 
-        val iconLayout = LinearLayout(context)
-        iconLayout.layoutParams = LinearLayout.LayoutParams(
-            0,
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            1f
-        )
-        iconLayout.setPadding(0, 16, 0, 0)
-        val carIcon = ImageView(context)
-        carIcon.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        ).apply {
-            gravity = Gravity.CENTER
-        }
+        val carIcon: ImageView = resultLayout.findViewById(R.id.transport_mode_icon)
         carIcon.setImageDrawable(
             ResourcesCompat.getDrawable(
                 resources,
                 iconResId!!,
                 context?.theme
             ))
-        iconLayout.addView(carIcon)
 
-        val descLayout = LinearLayout(context)
-        descLayout.layoutParams = LinearLayout.LayoutParams(
-            0,
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            4f
-        )
-        descLayout.orientation = LinearLayout.VERTICAL
-        val summaryText = MaterialTextView(requireContext())
+        val summaryText: TextView = resultLayout.findViewById(R.id.summary_text)
         summaryText.text = "via ${route.summary}"
-        summaryText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18f)
-        descLayout.addView(summaryText)
-//        val tapForMoreText = MaterialTextView(requireContext())
-//        tapForMoreText.text = getString(R.string.tap_for_more_details)
-//        tapForMoreText.setTextColor(resources.getColor(com.google.android.libraries.places.R.color.quantum_googblue, context?.theme))
-//        tapForMoreText.layoutParams = LinearLayout.LayoutParams(
-//            LinearLayout.LayoutParams.WRAP_CONTENT,
-//            LinearLayout.LayoutParams.WRAP_CONTENT
-//        )
-//        (tapForMoreText.layoutParams as LinearLayout.LayoutParams).apply {
-//            gravity = Gravity.BOTTOM
-//        }
-//        descLayout.addView(tapForMoreText)
 
-        val valuesLayout = LinearLayout(context)
-        valuesLayout.orientation = LinearLayout.VERTICAL
-        valuesLayout.layoutParams = LinearLayout.LayoutParams(
-            0,
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            2f
-        )
-        val emissionText = MaterialTextView(requireContext())
+        val emissionText: TextView = resultLayout.findViewById(R.id.emission_text)
         emissionText.text = CalculationUtils.calculateByDistance(route.legs[0].distance.inMeters, factor)
-        emissionText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18f)
-        val distText = MaterialTextView(requireContext())
+        val distText: TextView = resultLayout.findViewById(R.id.distance_text)
         distText.text = route.legs[0].distance.humanReadable
-        distText.alpha = 0.5f
-        val durationText = MaterialTextView(requireContext())
+        Log.i("distance testing", route.legs[0].distance.humanReadable)
+        val durationText: TextView = resultLayout.findViewById(R.id.duration_text)
         durationText.text = route.legs[0].duration.humanReadable
-        durationText.alpha = 0.5f
+        Log.i("duration testing", route.legs[0].duration.humanReadable)
+
         emissionTexts.add(emissionText)
         distTexts.add(distText)
         durationTexts.add(durationText)
-        valuesLayout.addView(emissionText)
-        valuesLayout.addView(distText)
-        valuesLayout.addView(durationText)
-
-        resultLayout.addView(iconLayout)
-        resultLayout.addView(descLayout)
-        resultLayout.addView(valuesLayout)
 
         mainLayout.addView(resultLayout)
     }

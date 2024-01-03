a86153ceca8e02165f4f8c8aff641dd83a58762b
Isac Kim <isac08270@gmail.com>
2024-01-04 00:32:00 +1100

    separated airplane mode

--- app/src/main/java/com/mquniversity/tcct/TransportSelection.kt
+++ app/src/main/java/com/mquniversity/tcct/TransportSelection.kt
@@ -1,23 +1,32 @@
 package com.mquniversity.tcct
 
 import android.content.Context
-import android.graphics.drawable.GradientDrawable
 import android.view.ContextThemeWrapper
 import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.button.MaterialButton
 
 class TransportSelection(mainActivityContext: Context): RadioGroup(mainActivityContext) {
+    private val mainActivity: MainActivity = mainActivityContext as MainActivity
     var currMode: TransportMode = TransportMode.CAR
 
-    private val btns: Array<RadioButton> = Array(TransportMode.entries.size) {
+    private val radioBtns: Array<RadioButton> = Array(TransportMode.entries.size - 1) {
         RadioButton(ContextThemeWrapper(
             context,
-            com.google.android.material.R.style.Widget_Material3_Button_TextButton_Icon
+            com.google.android.material.R.style.Widget_Material3_Button_IconButton_Outlined
         ))
     }
+    private val airplaneBtn = MaterialButton(
+        ContextThemeWrapper(
+            context,
+            com.google.android.material.R.style.Widget_Material3_Button_IconButton_Outlined
+        )
+    )
+
     private val icons: IntArray = intArrayOf(
         R.drawable.outline_directions_car_24,
         R.drawable.outline_sports_motorsports_24,
@@ -30,38 +39,54 @@ class TransportSelection(mainActivityContext: Context): RadioGroup(mainActivityC
             ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
-        this.setPadding(16, 0, 16, 0)
-        val shapeDivider = GradientDrawable()
-        shapeDivider.setSize(16, 0)
-        this.dividerDrawable = shapeDivider
-        this.showDividers = SHOW_DIVIDER_MIDDLE
 
-        for (i in TransportMode.entries.indices) {
-            this.addView(btns[i])
-            val btnLayoutParams = LayoutParams(
+        for (i in radioBtns.indices) {
+            this.addView(radioBtns[i])
+            radioBtns[i].layoutParams = LayoutParams(
                 LayoutParams.WRAP_CONTENT,
                 LayoutParams.WRAP_CONTENT
-            )
-            btnLayoutParams.setMargins(16, 16, 16, 16)
-            btns[i].layoutParams = btnLayoutParams
-            btns[i].buttonDrawable = ResourcesCompat.getDrawable(
+            ).apply {
+                setMargins(16, 16, 16, 16)
+            }
+            radioBtns[i].buttonDrawable = ResourcesCompat.getDrawable(
                 resources,
                 icons[i],
                 context.theme
             )
-            btns[i].setOnCheckedChangeListener { _, isChecked ->
+            radioBtns[i].setTextColor(ContextCompat.getColorStateList(context, R.color.transport_foreground_color))
+            radioBtns[i].backgroundTintList = ContextCompat.getColorStateList(context, R.color.transport_background_color)?.withAlpha(97)
+            radioBtns[i].setBackgroundResource(R.drawable.transport_background_shape)
+            radioBtns[i].setOnCheckedChangeListener { _, isChecked ->
                 if (isChecked) {
                     currMode = TransportMode.entries[i]
                 }
             }
-            btns[i].setTextColor(ContextCompat.getColorStateList(context, R.color.transport_foreground_color))
-            btns[i].backgroundTintList = ContextCompat.getColorStateList(context, R.color.transport_background_color)?.withAlpha(97)
-            btns[i].setBackgroundResource(R.drawable.transport_background_shape)
         }
-        btns[currMode.ordinal].isChecked = true
 
+        // special case for airplane
+        this.addView(airplaneBtn)
+        airplaneBtn.layoutParams = LayoutParams(
+            LayoutParams.WRAP_CONTENT,
+            LayoutParams.WRAP_CONTENT
+        ).apply {
+            setMargins(16, 16, 16, 16)
+        }
+        airplaneBtn.icon = ResourcesCompat.getDrawable(
+            resources,
+            icons[TransportMode.AIRPLANE.ordinal],
+            context.theme
+        )
+        airplaneBtn.setOnClickListener {
+            mainActivity.supportFragmentManager.beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .add(android.R.id.content, AirplaneQueryFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        radioBtns[currMode.ordinal].isChecked = true
         this.setOnCheckedChangeListener { _, _ ->
-            (mainActivityContext as MainActivity).calculate()
+            mainActivity.calculate()
         }
     }
 }

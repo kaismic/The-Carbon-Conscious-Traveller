1796dc81336e11a0c96bf36e0c131650ff4277ad
Isac Kim <isac08270@gmail.com>
2024-01-12 20:26:48 +1100

    removed calculateByDistance method

--- app/src/main/java/com/mquniversity/tcct/CalculationUtils.kt
+++ app/src/main/java/com/mquniversity/tcct/CalculationUtils.kt
@@ -1,14 +1,6 @@
 package com.mquniversity.tcct
 
 object CalculationUtils {
-    /**
-     * @return formatted text of CO2e emission in kilogram or gram
-     */
-    fun calculateByDistance(distInMeter: Long, factorKgKm: Float): String {
-        val emissionInGram: Float = distInMeter * factorKgKm
-        return formatEmissionWithCO2(emissionInGram, true)
-    }
-
     fun formatEmission(emissionInGram: Float): String {
         if (emissionInGram >= 1000) {
             return String.format("%.2f", emissionInGram / 1000) + " kg"
